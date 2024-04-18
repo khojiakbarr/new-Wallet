12169b33b41137d1025c189b3bdf08d623be1ffe
khojiakbarr <chinozbekov09@gmail.com>
2024-04-18 16:05:40 +0500

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
 import "./App.css";
-import CurrencyConverter from "./components/CurrencyConverter/CurrencyConverter";
-import { Routes, Route } from "react-router-dom";
-import History from "./components/History/History";
+
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<CurrencyConverter />} />
-      <Route path="/history" element={<History />} />
-    </Routes>
-  );
+  return <h1>hello</h1>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,17 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import CurrencyContext from "./context/CurrencyContext";
-import HistoryContex from "./context/HistoryContex";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-    <HistoryContex>
-      <CurrencyContext>
-        <App />
-      </CurrencyContext>
-    </HistoryContex>
-  </BrowserRouter>
-);
+root.render(<App />);
